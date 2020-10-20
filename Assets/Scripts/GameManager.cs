12bdf0ac0ac509721dc646113cb0cffa3b094774
Andrej <glazba@gmail.com>
2020-10-20 22:00:47 +0200

    win condition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
 	public Text[] buttonArray;
+	private string playerSide;
 
 	private void Awake() {
+		playerSide = "X";
 		SetGameManagerReferenceOnButtons();
 	}
 
@@ -18,11 +20,70 @@ public class GameManager : MonoBehaviour
 	}
 
 	public string GetPlayerSide() {
-		return "?";
+		return playerSide;
 	}
 
 	public void EndTurn() {
-		Debug.Log("EndTurn is not implemented!");
+		// Debug.Log("EndTurn is not implemented!");
+
+		if (buttonArray[0].text == playerSide &&
+			buttonArray[1].text == playerSide &&
+			buttonArray[2].text == playerSide) {
+			GameOver();
+		}
+		else if (
+			buttonArray[3].text == playerSide &&
+			buttonArray[4].text == playerSide &&
+			buttonArray[5].text == playerSide) {
+			GameOver();
+		}
+		else if (
+			buttonArray[6].text == playerSide &&
+			buttonArray[7].text == playerSide &&
+			buttonArray[8].text == playerSide) {
+			GameOver();
+		}
+		else if (
+			buttonArray[0].text == playerSide &&
+			buttonArray[3].text == playerSide &&
+			buttonArray[6].text == playerSide) {
+			GameOver();
+		}
+		else if (
+			buttonArray[1].text == playerSide &&
+			buttonArray[4].text == playerSide &&
+			buttonArray[7].text == playerSide) {
+			GameOver();
+		}
+		else if (
+			buttonArray[2].text == playerSide &&
+			buttonArray[5].text == playerSide &&
+			buttonArray[8].text == playerSide) {
+			GameOver();
+		}
+		else if (
+			buttonArray[0].text == playerSide &&
+			buttonArray[4].text == playerSide &&
+			buttonArray[8].text == playerSide) {
+			GameOver();
+		}
+		else if (
+			buttonArray[2].text == playerSide &&
+			buttonArray[4].text == playerSide &&
+			buttonArray[6].text == playerSide) {
+			GameOver();
+		}
+		ChangeSides();
+	}
+
+	void GameOver() {
+		for (int i = 0; i < buttonArray.Length; i++) {
+			buttonArray[i].GetComponentInParent<Button>().interactable = false;
+		}
+	}
+
+	void ChangeSides() {
+		playerSide = (playerSide == "X") ? "O" : "X";
 	}
 
 }
